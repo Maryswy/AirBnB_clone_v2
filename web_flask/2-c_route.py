be2aa518b4db02cbb4e1491f8e12c38d41745eb6
Maryswy <marysowa54@gmail.com>
2023-04-21 18:58:58 +0300

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
+"""Start web application with two routings
+"""
+
 from flask import Flask
-"""
-intializing flask web app to listen on 0.0.0.0:5000
-"""
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
 
 @app.route('/')
-def hello_world():
+def hello():
+    """Return string when route queried
     """
-    Display "Hello HBNB!"
-    """
-    return ('Hello HBNB!')
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
+    """Return string when route queried
     """
-    route /hbnb displays
-    """
-    return ('HBNB')
+    return 'HBNB'
+
 
 @app.route('/c/<text>')
-def c(text):
+def c_is_fun(text):
+    """Return reformatted text
     """
-    replaces _ with spaces
-    """
-    return ("C {}".format(text.replace('_', ' ')))
+    return 'C ' + text.replace('_', ' ')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
