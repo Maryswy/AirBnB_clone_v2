d218fde39f949bf5e0d19940fc944dea884c0946
Maryswy <marysowa54@gmail.com>
2023-04-22 00:02:58 +0300

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""Start web application with two routings
 """
-intializing flask web app to listen on 0.0.0.0:5000
-"""
+
+from models import storage
+from models.state import State
 from flask import Flask, render_template
-from models import storage, classes
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def states_list():
+    """Render template with states
     """
-    Displays html plage with states listed
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('7-states_list.html', states=states))
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
+def app_teardown(arg=None):
+    """Clean-up session
     """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
