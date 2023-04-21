2c6b7725ca1ff7271e30121e20eb8b04e0b5275f
Maryswy <marysowa54@gmail.com>
2023-04-22 00:12:44 +0300

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""Start web application with two routings
 """
-intializing flask web app to listen on 0.0.0.0:5000
-"""
+
+from models import storage
+from models.state import State
 from flask import Flask, render_template
-from models import storage, classes
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/states_list')
-def states_list():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
 
 
 @app.route('/cities_by_states')
-def cities_by_states():
+def states_list():
+    """Render template with states
     """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
+def app_teardown(arg=None):
+    """Clean-up session
     """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    if __name__ == '__main__':
+        app.url_map.strict_slashes = False
+        app.run(host='0.0.0.0', port=5000)
